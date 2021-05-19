7fe0e550e7de7350f8c98e3cf7cf37690aed8812
DenTaku <dentaku1412@gmail.com>
2021-05-19 16:00:22 +0900

    Done kadai42

---
+++ cpro2/kadai42.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define M 3
+#define N 4
+
+int main() {
+    int a[M][N];
+    int i, j;
+
+    for(i = 0; i < M; ++i) {
+        for(j = 0; j < N; ++j) {
+            a[i][j] = 10 * i + j;
+            printf("%2d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
