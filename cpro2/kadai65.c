a6232b16df1cf5f0e7ae5df03f05de7a48192251
DenTaku <dentaku1412@gmail.com>
2021-06-02 17:15:14 +0900

    Done kadai65

---
+++ cpro2/kadai65.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void print(int m, int n, const float *x);
+void print_oct(int m, int n, const float *x, const char * name);
+void add(int n, const float *x, float *o);
+void scale(int n, float x, float *o);
+void init(int n, float x, float *o);
+
+int main() {
+    float y[6];
+    print_oct(2, 3, y, "y");
+    init(6, 7, y);
+    print_oct(2, 3, y, "y");
+    return 0;
+}
+
+void print(int m, int n, const float *x) {
+    int i, j;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            printf("%.4f ", x[i * n + j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_oct(int m, int n, const float *x, const char * name) {
+    int i, j;
+    printf("%s = [", name);
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j) {
+            printf(" %.4f ", x[i * n + j]);
+        }
+        printf(";\n");
+    }
+    printf("];\n");
+}
+
+void add(int n, const float *x, float *o) {
+    int i;
+    for (i = 0; i < n; ++i) {
+        o[i] += x[i];
+    }
+}
+
+void scale(int n, float x, float *o) {
+    int i;
+    for (i = 0; i < n; ++i) {
+        o[i] *= x;
+    }
+}
+
+void init(int n, float x, float *o) {
+    int i;
+    for (i = 0; i < n; ++i) {
+        o[i] = x;
+    }
+}
