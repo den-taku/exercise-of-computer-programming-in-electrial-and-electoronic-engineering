a0e68697370f220bd6a1c1b22a5a7a11738a3045
DenTaku <dentaku1412@gmail.com>
2021-06-09 16:31:47 +0900

    Done kadai71

---
+++ cpro2/kadai71.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int len = 0;
+    char str[128];
+    printf("input a word: ");
+    scanf("%s", str);
+    while (str[len]) {
+        if (97 <= str[len] && str[len] <= 122) {
+            str[len] = ((str[len] - 97 + 1) % 26) + 97;
+        } else if (65 <= str[len] && str[len] <= 90) {
+            str[len] = ((str[len] - 65 + 1) % 26) + 65;
+        }
+        ++len;
+    }
+    printf("%s\n", str);
+}
