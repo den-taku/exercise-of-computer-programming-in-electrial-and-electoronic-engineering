e9bb50e83973b562fd23c32a00370a8d8a5dbeb6
den-taku <dentaku1412@gmail.com>
2021-07-01 07:05:46 +0900

    Add comments

--- cpro2/data-samplecode-v11/kadai.c
+++ cpro2/data-samplecode-v11/kadai.c
@@ -425,6 +425,7 @@ int main(int argc, char *argv[]) {
 //     }
 // }
 
+// calculate y = Ax + b, A: m x n matrix.
 inline static void fc(int m, int n, const float *x, const float *A, const float *b, float *y) {
     int i, j;
     for (i = 0; i < m; ++i) {
@@ -435,12 +436,14 @@ inline static void fc(int m, int n, const float *x, const float *A, const float 
     } 
 }
 
+// relu: _/
 inline static void relu(int n, const float *x, float *y) {
     rep(i, n) {
         y[i] = (x[i] > 0.0) ? x[i] : 0.0;
     }
 }
 
+// find max element from data.
 inline static float max(int n, const float *data) {
     float max = data[0];
 	rep(i, n) {
@@ -451,6 +454,7 @@ inline static float max(int n, const float *data) {
     return max;
 }
 
+// softmax: y_k = exp(x_k) / Σi exp(x_i)
 inline static void softmax(int n, const float *x, float *y) {
     float mx = max(n, x); // Θ(n)
     float all = 0;
@@ -464,6 +468,7 @@ inline static void softmax(int n, const float *x, float *y) {
     }
 }
 
+// find max index from data.
 inline static size_t max_index(int n, const float *data) {
     float max = data[0];
     size_t max_index = 0;
@@ -476,6 +481,7 @@ inline static size_t max_index(int n, const float *data) {
     return max_index;
 }
 
+// infer x: calulare fc -> relu -> softmax, then return max{y_i}
 inline static int inference3(const float *A, const float *b, const float *x, float *y) {
     float *y_tmp = malloc(sizeof(float) * 10);
     // A: 10x784, b: 10, x: 784
@@ -487,18 +493,20 @@ inline static int inference3(const float *A, const float *b, const float *x, flo
     return max;
 }
 
+// softmax and loss's backward: ∂E/∂x_k = y_k - t_k
 inline static void softmaxwithloss_bwd(int n, const float *y, unsigned char t, float *dEdx) {
     copy(n, y, dEdx);
     dEdx[t] -= 1.0;
 }
 
+// relu's backward: ∂E/∂x_k = _/ * ∂E/∂y_k
 inline static void relu_bwd(int n, const float *x, const float *dEdy, float *dEdx) {
     rep(i, n) {
         dEdx[i] = (x[i] > 0.0) ? dEdy[i] : 0.0;
     }
 }
 
-// no tests
+// fc's backward: ∂E/∂x_k = a_k^T * ∂E/∂y, ∂E/∂a_k = ∂E/∂y_k * x^T, ∂E/∂b_k = ∂E/∂y_k
 inline static void fc_bwd(int m, int n, const float *x, const float *dEdy, const float *A, float *dEdA, float *dEdb, float *dEdx){
     rep(i, m) {
         rep(j, n) {
@@ -517,12 +525,14 @@ inline static void fc_bwd(int m, int n, const float *x, const float *dEdy, const
     }
 }
 
+// copy array.
 inline static void copy(int n, const float *x, float *y) {
     rep(i, n) {
         y[i] = x[i];
     }
 }
 
+// 3 layer's backward: fc -> relu -> softmax, return ∂E/∂a_k, ∂E/∂b_k, ∂E/∂x_k
 inline static void backward3(const float *A, const float *b, const float *x, unsigned char t, float *y, float *dEdA, float *dEdb) {
     // A: 10x784, b: 10, x: 784
     float *relu_x = malloc(sizeof(float) * 10);
@@ -542,13 +552,14 @@ inline static void backward3(const float *A, const float *b, const float *x, uns
     free(dEdx);
 }
 
+// swap x and y.
 inline static void swap_int(int *x, int *y) {
     int tmp = *x;
     *x = *y;
     *y = tmp;
 }
 
-// no tests
+// shuffle array's element.
 inline static void shuffle(int n, int *x) {
     rep(i, n) {
         int j = (int)rand() % n;
@@ -556,35 +567,40 @@ inline static void shuffle(int n, int *x) {
     }
 }
 
-// no tests
+// calculate cross entoropy error with y and t.
 inline static float cross_entoropy_error(const float *y, int t) {
     return -log(y[t] + 1e-7);
 }
 
+// calculate o += x
 inline static void add(int n, const float *x, float *o) {
     rep(i, n) {
         o[i] += x[i];
     }
 }
 
+// calculate o *= x
 inline static void scale(int n, float x, float *o) {
     rep(i, n) {
         o[i] *= x;
     }
 }
 
+// set x in o.
 inline static void init(int n, float x, float *o) {
     rep(i, n) {
         o[i] = x;
     }
 }
 
+// set random value in o: value ∈ [-1, 1].
 inline static void rand_init(int n, float *o) {
     rep(i, n) {
         o[i] = (float)rand() / RAND_MAX * 2.0 - 1.0;
     }
 }
 
+// infer x: calulare fc -> relu -> fc -> relu -> fc -> softmax, then return max{y_i}
 inline static int inference6(const float *A1, const float *b1, const float *A2, const float *b2, const float *A3, const float *b3, const float *x, float *y) {
     // A1: 50x784, b1: 50, x: 784
     // A2: 100x50, b2: 100,
@@ -605,6 +621,7 @@ inline static int inference6(const float *A1, const float *b1, const float *A2, 
     return max;
 }
 
+// 6 layer's backward: fc -> relu -> softmax, return ∂E/∂a_k x 3, ∂E/∂b_k x 3, ∂E/∂x_k
 inline static void backward6(const float *A1, const float *b1, const float *A2, const float *b2, const float *A3, const float *b3, const float *x, unsigned char t, float *y, float *dEdA1, float *dEdb1, float *dEdA2, float *dEdb2, float *dEdA3, float *dEdb3) {
     // A1: 50x784, b1: 50, x: 784
     // A2: 100x50, b2: 100,
@@ -646,6 +663,7 @@ inline static void backward6(const float *A1, const float *b1, const float *A2, 
     free(dEdx);
 }
 
+// save A: mxn, b: n -> filename.
 inline static void save(const char *filename, int m, int n, const float *A, const float *b) {
     FILE *fp;
     fp = fopen(filename, "wr");
@@ -658,6 +676,7 @@ inline static void save(const char *filename, int m, int n, const float *A, cons
     fclose(fp);
 }
 
+// load A: mxn, b: n <- filename.
 inline static void load(const char *filename, int m, int n, float *A, float *b) {
     FILE *fp;
     fp = fopen(filename, "rb");
