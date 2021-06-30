b68de7815329ad237bf21b9dfc31cec1b1859ac2
den-taku <dentaku1412@gmail.com>
2021-07-01 06:26:58 +0900

    Check

--- cpro2/data-samplecode-v11/kadai.c
+++ cpro2/data-samplecode-v11/kadai.c
@@ -31,22 +31,22 @@ static void load(const char *filename, int m, int n, float *A, float *b);
 
 int main(int argc, char *argv[]) {
     srand((unsigned int)time(NULL));
-	float *train_x = NULL;
-	unsigned char *train_y = NULL;
-	int train_count = -1;
-	float *test_x = NULL;
-	unsigned char *test_y = NULL;
-	int test_count = -1;
-	int width = -1;
-	int height = -1;
-	load_mnist(&train_x, &train_y, &train_count, 
-		&test_x, &test_y, &test_count, 
-		&width, &height);
-	#if 0
-	  volatile float x = 0;
-	  volatile float y = 0;
-	  volatile float z = x/y;
-	#endif
+    float *train_x = NULL;
+    unsigned char *train_y = NULL;
+    int train_count = -1;
+    float *test_x = NULL;
+    unsigned char *test_y = NULL;
+    int test_count = -1;
+    int width = -1;
+		int height = -1;
+		load_mnist(&train_x, &train_y, &train_count,
+							 &test_x, &test_y, &test_count,
+							 &width, &height);
+#if 0
+      volatile float x = 0;
+      volatile float y = 0;
+      volatile float z = x/y;
+    #endif
 
 	const int epoch = 10;
 	const int mini_batch = 100;
